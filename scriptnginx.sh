4afbee982af9d3543e829a4bd7218e2ca7b37ece
Raul <114144263+raulqlda@users.noreply.github.com>
2022-11-14 09:51:08 +0100

    Add files via upload

---
+++ scriptnginx.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y php
+sudo apt install -y nginx  
+sudo apt install -y php-mysql   
+sudo apt install -y php-mbstring
+sudo apt install -y php-zip
+sudo apt install -y php-gd
+sudo apt install -y php-json
+sudo apt install -y php-curl  
+sudo apt install -y wget
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php
